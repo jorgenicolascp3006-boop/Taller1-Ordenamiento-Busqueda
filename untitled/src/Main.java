24b1e0cf1827a501e2fca3f9fba37d68ec5b491d
jorgenicolascp3006-boop <jorgenicolascp3006@gmail.com>
2026-08-29 20:58:35 -0500

    update definitivo
    Subi todo actualice el main y opera todo al tiempo jajajaj (:

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
+        int [] miArray = {70,12,19,45,11,33,26};
+        MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
 
-        int [] skibidiArray = {70,12,19,45,11,33,26};
-        //int [] skibidiArray = {11,12,19,26,33,45,70};
-        MisAlgoritmos misalgoritmos = new MisAlgoritmos();
-
-        //System.out.println(Arrays.toString(misalgoritmos.selectionSort(skibidiArray)));
-        System.out.println(Arrays.toString(misalgoritmos.insertionSort(skibidiArray)));
-
+        System.out.println("Lineal: " + misAlgoritmos.linearSearch(miArray, 45));
+        System.out.println("Ordenado por bubbleSort: " + java.util.Arrays.toString(misAlgoritmos.bubbleSort(miArray)));
+        System.out.println("Binaria: " + misAlgoritmos.binarySearch(miArray, 70)); // miArray ya se ordenó arriba
+        System.out.println("Ordenado por selectionSort: " + java.util.Arrays.toString(misAlgoritmos.selectionSort(miArray)));
+        System.out.println("Ordenado por insertionSort: " + java.util.Arrays.toString(misAlgoritmos.insertionSort(miArray)));
+        System.out.println("Ordenado por mergeSort: " + java.util.Arrays.toString(misAlgoritmos.mergeSort(miArray)));
+        System.out.println("Ordenado por quickSort: " + java.util.Arrays.toString(misAlgoritmos.quickSort(miArray)));
     }
-
 }
--- untitled/src/MisAlgoritmos.java
+++ untitled/src/MisAlgoritmos.java
@@ -94,11 +94,89 @@ public class MisAlgoritmos  implements Busquedas,Ordenamientos{
 
     @Override
     public int[] mergeSort(int[] arrayDesordenado) {
-        return new int[0];
+        if (arrayDesordenado == null || arrayDesordenado.length <= 1) {
+            return arrayDesordenado;
+        }
+
+        int medio = arrayDesordenado.length / 2;
+
+        int[] izquierda = new int[medio];
+        int[] derecha = new int[arrayDesordenado.length - medio];
+
+        System.arraycopy(arrayDesordenado, 0, izquierda, 0, medio);
+        System.arraycopy(arrayDesordenado, medio, derecha, 0, arrayDesordenado.length - medio);
+
+        int[] izquierdaOrdenada = mergeSort(izquierda);
+        int[] derechaOrdenada = mergeSort(derecha);
+
+        return merge(izquierdaOrdenada, derechaOrdenada);
     }
+
+    private int[] merge(int[] izquierda, int[] derecha) {
+        int[] resultado = new int[izquierda.length + derecha.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < izquierda.length && j < derecha.length) {
+            if (izquierda[i] <= derecha[j]) {
+                resultado[k++] = izquierda[i++];
+            } else {
+                resultado[k++] = derecha[j++];
+            }
+        }
+        while (i < izquierda.length) {
+            resultado[k++] = izquierda[i++];
+        }
+
+        while (j < derecha.length) {
+            resultado[k++] = derecha[j++];
+        }
+        return resultado;
+    }
+
+
 
     @Override
     public int[] quickSort(int[] arrayDesordenado) {
-        return new int[0];
+        if (arrayDesordenado == null || arrayDesordenado.length <= 1) {
+            return arrayDesordenado;
+        }
+
+        int pivote = arrayDesordenado[arrayDesordenado.length - 1];
+
+        int menores = 0;
+        int mayores = 0;
+        for (int i = 0; i < arrayDesordenado.length - 1; i++) {
+            if (arrayDesordenado[i] <= pivote) {
+                menores++;
+            } else {
+                mayores++;
+            }
+        }
+        int[] menor = new int[menores];
+        int[] mayor = new int[mayores];
+
+        int iMenores = 0;
+        int iMayores = 0;
+
+        for (int i = 0; i < arrayDesordenado.length - 1; i++) {
+            if (arrayDesordenado[i] <= pivote) {
+                menor[iMenores++] = arrayDesordenado[i];
+            } else {
+                mayor[iMayores++] = arrayDesordenado[i];
+            }
+        }
+
+        int[] menoresOrdenados = quickSort(menor);
+        int[] mayoresOrdenados = quickSort(mayor);
+
+        int[] resultado = new int[arrayDesordenado.length];
+
+        System.arraycopy(menoresOrdenados, 0, resultado, 0, menoresOrdenados.length);
+        resultado[menoresOrdenados.length] = pivote;
+        System.arraycopy(mayoresOrdenados, 0, resultado, menoresOrdenados.length + 1, mayoresOrdenados.length);
+
+        return resultado;
+
     }
 }
+
